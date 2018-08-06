ae15ca5ca095f4460f7e77653e5a3cb5e63130f4
fad <fnassiri@vente-privee.com>
2018-08-06 08:35:05 +0200

    feat(server): add headers to requests

--- server.js
+++ server.js
@@ -8,8 +8,23 @@ const bodyParser = require('body-parser');
 const log4js = require('log4js');
 
 log4js.configure({
-  appenders: { fileLogger: { type: 'file', filename: './logs/snack.log' , backups: 10, maxLogSize: 50000000 }, console: { type: 'console'} },
-  categories: { default: { appenders: ['fileLogger', 'console'], level: 'info' } }
+  appenders: {
+    fileLogger: {
+      type: 'file',
+      filename: './logs/snack.log',
+      backups: 10,
+      maxLogSize: 50000000
+    },
+    console: {
+      type: 'console'
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['fileLogger', 'console'],
+      level: 'info'
+    }
+  }
 });
 
 const logger = log4js.getLogger();
@@ -30,7 +45,25 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO }));
+app.use(log4js.connectLogger(logger, {
+  level: log4js.levels.INFO
+}));
+
+
+// Add headers
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers', ['X-Requested-With, content-type', 'x-access-token']);
+
+  if ('OPTIONS' === req.method) {
+    return res.send(200);
+  }
+
+  next();
+});
 
 app.use('/places', security.verifyToken, placesRouter);
 app.use('/users', security.verifyToken, usersRouter);
